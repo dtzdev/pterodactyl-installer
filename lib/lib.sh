8d7f4b3a3e5bf601297a41ddbe63207e3549a697
dtz <dtz@dtzdev.xyz>
2024-08-08 01:28:57 +0300

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -51,7 +51,7 @@ export SUPPORTED=false
 # download URLs
 export PANEL_DL_URL="https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz"
 export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
-export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
+export MARIADB_URL="https://r.mariadb.com/downloads/mariadb_repo_setup"
 export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
